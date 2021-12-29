7b94498ce0b82c52fa983b742a704d1dd35389f4
ridaafzalkhan <ridaafzal0@gmail.com>
2021-12-29 20:55:04 +0500

    home modified

--- app/src/main/java/rida/org/makharijal_huruf/Home.java
+++ app/src/main/java/rida/org/makharijal_huruf/Home.java
@@ -2,6 +2,7 @@ package rida.org.makharijal_huruf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Home extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+       Intent intent = new Intent(Home.this, MainActivity3.class);
+        intent.putExtra("btn","lahatiyah");
+        startActivity(intent);
     }
 }
